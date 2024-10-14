615a55aaa839568832f7ee960c7607a10d4a0d5c
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2024-10-14 23:55:14 +1100

    refactor: Add missing annotations

--- src/main/java/com/csci318/microservice/order/Domain/Entities/OrderItem.java
+++ src/main/java/com/csci318/microservice/order/Domain/Entities/OrderItem.java
@@ -1,10 +1,18 @@
 package com.csci318.microservice.order.Domain.Entities;
 
-import jakarta.persistence.*;
+import com.csci318.microservice.order.Domain.Relations.Item;
+import com.csci318.microservice.order.Domain.Relations.Restaurant;
+import com.csci318.microservice.order.Utils.Annotations.ManyToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.util.UUID;
 
@@ -20,12 +28,15 @@ public class OrderItem {
     private UUID id;
 
     @Column(name = "order_id")
+    @ManyToOne(targetEntity = Order.class)
     private UUID orderId;
 
     @Column(name = "restaurant_id")
+    @ManyToOne(targetEntity = Restaurant.class)
     private UUID restaurantId;
 
     @Column(name = "item_id")
+    @ManyToOne(targetEntity = Item.class)
     private UUID itemId;
 
     @Column(name = "quantity")
